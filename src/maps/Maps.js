269e174b6ced6a0551058e2b6f31c5a5b47f4640
anshusaurav <anshu.saurav@gmail.com>
2020-09-04 21:32:03 +0530

    Location marker

--- src/maps/Maps.js
+++ src/maps/Maps.js
@@ -1,70 +1,4 @@
-// import React from "react";
-// import { Map, GoogleApiWrapper, Marker } from "google-maps-react";
 
-// const apiMaps = "AIzaSyA5UEAy2FQgtkK3FaVOfnFFp9hVNAxxQTM";
-
-// const mapStyles = {
-//     width: "90%",
-//     heigth: "100%"
-// };
-
-// export class MapContainer extends React.Component {
-//     constructor(props) {
-//         super(props);
-
-//         this.state = {
-//             venues: [{ lat: 32.219044, lng: 76.323402 }],
-//             initLat: 30.55,
-//             initLong: 67.99
-//         };
-//     }
-//     componentDidMount() {
-//         if ("geolocation" in navigator) {
-//             console.log("Available");
-//             navigator.geolocation.getCurrentPosition((position) => {
-//                 console.log("Latitude is :", position.coords.latitude);
-//                 console.log("Longitude is :", position.coords.longitude);
-//                 // this.setState({ initLat: position.coords.latitude, initLong: position.coords.longitude })
-//             });
-//         } else {
-//             console.log("Not Available");
-//         }
-//     }
-
-
-//     displayMarkers = () => {
-//         return this.state.venues.map((venue, index) => {
-//             return (
-//                 <Marker
-//                     key={index}
-//                     id={index}
-//                     position={{
-//                         lat: venue.latitude,
-//                         lng: venue.longitude
-//                     }}
-//                     onClick={() => console.log(this.state.venues)}
-//                 />
-//             );
-//         });
-//     };
-
-//     render() {
-//         return (
-//             <Map
-//                 google={this.props.google}
-//                 zoom={17}
-//                 style={mapStyles}
-//                 initialCenter={{ lat: 32.219044, lng: 76.323402 }}
-//             >
-//                 {this.displayMarkers()}
-//             </Map>
-//         );
-//     }
-// }
-
-// export default GoogleApiWrapper({
-//     apiKey: apiMaps
-// })(MapContainer);
 import React from "react";
 import { Map, GoogleApiWrapper, Marker } from "google-maps-react";
 
@@ -80,22 +14,10 @@ export class MapContainer extends React.Component {
         super(props);
 
         this.state = {
-            venues: [{ lat: 32.5, lng: 76 }, { lat: 33.5, lng: 76 }, { lat: 33, lng: 76 }, { lat: 34, lng: 76 }],
+            venues: [],
             lat: 32,
             lng: 76
         };
-    }
-    componentDidMount() {
-        if ("geolocation" in navigator) {
-            console.log("Available");
-            navigator.geolocation.getCurrentPosition((position) => {
-                console.log("Latitude is :", position.coords.latitude);
-                console.log("Longitude is :", position.coords.longitude);
-                this.setState({ lat: position.coords.latitude, lng: position.coords.longitude })
-            });
-        } else {
-            console.log("Not Available");
-        }
     }
     displayMarkers = () => {
         return this.state.venues.map((venue, index) => {
@@ -112,12 +34,37 @@ export class MapContainer extends React.Component {
             );
         });
     };
+    componentDidMount() {
+        if ("geolocation" in navigator) {
+            navigator.geolocation.getCurrentPosition((position) => {
+
+                console.log("Latitude is :", position.coords.latitude);
+                console.log("Longitude is :", position.coords.longitude);
+                this.setState(
+                    {
+                        venues: this.state.venues.concat([
+                            { lat: position.coords.latitude, lng: position.coords.longitude }
+                        ])
+                    },
+                    () => {
+                        console.log("Venues", this.state.venues);
+                    }
+                );
+            });
+
+
+        } else {
+            console.log("Not Available");
+        }
+    }
+
+
 
     render() {
         return (
             <Map
                 google={this.props.google}
-                zoom={12}
+                zoom={9}
                 style={mapStyles}
                 initialCenter={{ lat: this.state.lat, lng: this.state.lng }}
             >
